64d90d398776c5ce3473db8ecbf4344d4667f17d
Николай баранович <nick.fakov@yandex.ru>
2023-05-13 15:15:42 +0300

    new branch controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -12,15 +12,16 @@ import java.util.Collection;
 import java.util.HashMap;
 
 @Slf4j
+
 @RestController
 public class FilmController {
-    private HashMap<Integer, Film> Films = new HashMap<>();
+    private HashMap<Integer, Film> films = new HashMap<>();
     private int nextFilmId = 1;
 
     @GetMapping("/films") //получение всех фильмов.
     public Collection<Film> allFilms() {
-        log.info("оличество фильмов{}" + Films.size());
-        return Films.values();
+        log.info("оличество фильмов{}" + films.size());
+        return films.values();
     }
 
     @PostMapping("/films") // добавление фильма.
@@ -30,7 +31,7 @@ public class FilmController {
             throw new WeriOldFilmException("YYYY-MM-dd < 1895-12-28");
         } else {
             film.setId(nextFilmId++);
-            Films.put(film.getId(), film);
+            films.put(film.getId(), film);
             log.info("добавлен новый фильм " + film);
         }
         return film;
@@ -38,12 +39,12 @@ public class FilmController {
 
     @PutMapping("/films") // обновление фильма.
     public Film changeFilm(@Valid @RequestBody Film film) throws NotIdExeption, WeriOldFilmException {
-        if (Films.containsKey(film.getId())) {
+        if (films.containsKey(film.getId())) {
             if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
                 log.warn("ошибка при добовлении фильма " + film + " старше «L'Arrivée d'un train en gare de la Ciotat» 1895 , нечего нет ");
                 throw new WeriOldFilmException("YYYY-MM-dd < 1895-12-28");
             } else {
-                Films.put(film.getId(), film);
+                films.put(film.getId(), film);
                 log.info("фильм обновлён " + film);
             }
         } else {
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -12,14 +12,14 @@ import java.util.HashMap;
 @Slf4j
 @RestController
 public class UserController {
-    private HashMap<Integer, User> Users = new HashMap<>();
+    private HashMap<Integer, User> users = new HashMap<>();
 
     private int nextUserId = 1;
 
     @GetMapping("/users") // получение списка всех пользователей.
     public Collection<User> allUsers() {
-        log.info("всего пользывателей {} " + Users.size());
-        return Users.values();
+        log.info("всего пользывателей {} " + users.size());
+        return users.values();
 
 
     }
@@ -30,15 +30,15 @@ public class UserController {
             user.setName(user.getLogin());
         }
         user.setId(nextUserId++);
-        Users.put(user.getId(), user);
+        users.put(user.getId(), user);
         log.info("добавлен новый пользыватель " + user);
         return user;
     }
 
     @PutMapping("/users") // обновление пользователя.
     public User changeUser(@Valid @RequestBody User user) throws NotIdExeption {
-        if (Users.containsKey(user.getId())) {
-            Users.put(user.getId(), user);
+        if (users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
             log.info("обнавлён пользыватель " + user);
         } else {
             log.warn("ошибка обновления " + user + " не возможно изменить того чего нет");
