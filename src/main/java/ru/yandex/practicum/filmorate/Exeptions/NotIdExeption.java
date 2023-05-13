6e72fa8ef1bf73f6f6de38114dfcbded7d8e46b2
Николай баранович <nick.fakov@yandex.ru>
2023-05-13 15:03:46 +0300

    new branch controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/Exeptions/NotIdExeption.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exeptions/NotIdExeption.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 public class NotIdExeption extends ResponseStatusException {
-    public NotIdExeption(final String message){
+    public NotIdExeption(final String message) {
         super(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/Exeptions/WeriOldFilmException.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exeptions/WeriOldFilmException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 public class WeriOldFilmException extends ResponseStatusException {
-    public WeriOldFilmException(final String message){
+    public WeriOldFilmException(final String message) {
         super(HttpStatus.BAD_REQUEST);
     }
 }
