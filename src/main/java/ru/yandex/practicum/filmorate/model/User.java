8c85df64f6c5b7cdd2cf3dfbea0cbf20fa391c4b
Николай баранович <nick.fakov@yandex.ru>
2023-05-13 15:24:28 +0300

    new branch controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -10,15 +10,15 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class User {
-    private  int id ;
+    private int id;
     @NotBlank(message = "почта не может  быть пустой")
     @Email(message = "не коректо указанная почта")
-    private  String email ;
-@NotBlank(message = "логин не может быть пустым")
-    @Pattern(regexp = "^[a-zA-Z0-9а-яА-Я._-]{1,50}",message = "логин не может содержать пробыл и быть длиней чем 50 символов")
-    private  String login ; // логин пользователя
-    private  String name ; // имя для отображения
+    private String email;
+    @NotBlank(message = "логин не может быть пустым")
+    @Pattern(regexp = "^[a-zA-Z0-9а-яА-Я._-]{1,50}", message = "логин не может содержать пробыл и быть длиней чем 50 символов")
+    private String login; // логин пользователя
+    private String name; // имя для отображения
 
     @Past(message = "ты что из будущего ?")
-    private  LocalDate birthday ; // дата рождение
+    private LocalDate birthday; // дата рождение
 }
