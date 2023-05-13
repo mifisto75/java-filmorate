52eb4f80f33b1f01c3f8f32c1eb3d79bb8f36a07
Николай баранович <nick.fakov@yandex.ru>
2023-05-13 13:04:33 +0300

    new branch controllers-films-users

--- src/test/java/ru/yandex/practicum/filmorate/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserTest.java
@@ -1,50 +1,136 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import ru.yandex.practicum.filmorate.controller.UserController;
-import ru.yandex.practicum.filmorate.model.User;
+import org.springframework.test.web.servlet.MockMvc;
 
-import javax.validation.ValidationException;
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-
+@AutoConfigureMockMvc
 public class UserTest {
-    private User user;
-    private UserController userController;
 
-    @BeforeEach
-    public void beforeEach() {
-        userController = new UserController();
-        user = User.builder()
-                .email("mail@mail.ru")
-                .login("dolore")
-                .name("Nick Name")
-                .birthday(LocalDate.of(1946, 8, 20))
-                .build();
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void addUserOkTest() throws Exception {
+        mockMvc.perform(
+                        post("/users")
+                                .content("{\n" +
+                                        "  \"login\": \"dolore\",\n" +
+                                        "  \"name\": \"Nick Name\",\n" +
+                                        "  \"email\": \"mail@mail.ru\",\n" +
+                                        "  \"birthday\": \"1946-08-20\"\n" +
+                                        "}")
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isOk());
+    }
+
+    @Test
+    void addUserFailLoginTest() throws Exception {
+        mockMvc.perform(
+                post("/users")
+                        .content("{\n" +
+                                "  \"login\": \"dolore ullamco\",\n" +
+                                "  \"email\": \"yandex@mail.ru\",\n" +
+                                "  \"birthday\": \"2446-08-20\"\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is4xxClientError());
     }
     @Test
-    public void addUserOkTest() {
-        User user1 = userController.addUser(user);
-        assertEquals(user, user1);
-        assertEquals(1, userController.allUsers().size());
+    void addUserFailEmailTest() throws Exception {
+        mockMvc.perform(
+                post("/users")
+                        .content("{\n" +
+                                "  \"login\": \"dolore ullamco\",\n" +
+                                "  \"name\": \"\",\n" +
+                                "  \"email\": \"mail.ru\",\n" +
+                                "  \"birthday\": \"1980-08-20\"\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is4xxClientError());
+    }
+    @Test
+    void addUserFailBirthdayTest() throws Exception {
+        mockMvc.perform(
+                post("/users")
+                        .content("{\n" +
+                                "  \"login\": \"dolore\",\n" +
+                                "  \"name\": \"\",\n" +
+                                "  \"email\": \"test@mail.ru\",\n" +
+                                "  \"birthday\": \"2446-08-20\"\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is4xxClientError());
+    }
+    @Test
+    void UpdateUseOkTest() throws Exception {
+        mockMvc.perform(
+                post("/users")
+                        .content("{\n" +
+                                "  \"login\": \"dolore\",\n" +
+                                "  \"name\": \"Nick Name\",\n" +
+                                "  \"email\": \"mail@mail.ru\",\n" +
+                                "  \"birthday\": \"1946-08-20\"\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        mockMvc.perform(
+                put("/users")
+                        .content("{\n" +
+                                "  \"login\": \"doloreUpdate\",\n" +
+                                "  \"name\": \"est adipisicing\",\n" +
+                                "  \"id\": 1,\n" +
+                                "  \"email\": \"mail@yandex.ru\",\n" +
+                                "  \"birthday\": \"1976-09-20\"\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+    }
+    @Test
+    void UpdateUseUnknownTest() throws Exception {
+        mockMvc.perform(
+                post("/users")
+                        .content("{\n" +
+                                "  \"login\": \"dolore\",\n" +
+                                "  \"name\": \"Nick Name\",\n" +
+                                "  \"email\": \"mail@mail.ru\",\n" +
+                                "  \"birthday\": \"1946-08-20\"\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        mockMvc.perform(
+                put("/users")
+                        .content("{\n" +
+                                "  \"login\": \"doloreUpdate\",\n" +
+                                "  \"name\": \"est adipisicing\",\n" +
+                                "  \"id\": 9999,\n" +
+                                "  \"email\": \"mail@yandex.ru\",\n" +
+                                "  \"birthday\": \"1976-09-20\"\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is4xxClientError());
+    }
     }
 
- //   @Test
-  //  public void addUserFailLoginTest() {
 
-   //     user.setLogin("dolore ullamco");
-   //    userController.addUser(user) ;
-   //      assertThrows(ValidationException.class ,()-> userController.addUser(user));
-   //    assertEquals(0, userController.allUsers().size());
-   // }
-}
+
+
+
+
+
+
+
+
+
 
