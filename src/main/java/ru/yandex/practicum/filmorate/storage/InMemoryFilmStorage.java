6e7c63e616213e6cccfe328cdb671673e98dff85
Николай баранович <nick.fakov@yandex.ru>
2023-05-27 18:18:20 +0300

    new branch add-friends-likes

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -13,35 +13,35 @@ import java.util.List;
 
 
 @Component
-public class InMemoryFilmStorage implements FilmStorage{
+public class InMemoryFilmStorage implements FilmStorage {
 
     private HashMap<Integer, Film> films = new HashMap<>();
     private int nextFilmId = 1;
 
 @Override
-    public List<Film> allFilms() {// получение списка всех фильмов
+    public List<Film> allFilms(){ // получение списка всех фильмов
         return new ArrayList(films.values());
     }
 
     @Override
-    public Film addFilm(Film film) throws ValidationException {// создание фильма.
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+    public Film addFilm(Film film) throws ValidationException{ // создание фильма.
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))){
             throw new ValidationException("вы указали неверную дату . самый первый фильм был создан в 1895.12.28");
-        } else {
+        } else{
             film.setId(nextFilmId++);
             films.put(film.getId(), film);
         }
         return film;
     }
     @Override
-    public Film changeFilm(Film film)throws NotFoundException, ValidationException {// обновление фильма.
-        if (films.containsKey(film.getId())) {
-            if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+    public Film changeFilm(Film film)throws NotFoundException, ValidationException{// обновление фильма.
+        if (films.containsKey(film.getId())){
+            if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))){
                 throw new ValidationException("вы указали неверную дату . самый первый фильм был создан в 1895.12.28");
-            } else {
+            } else{
                 films.put(film.getId(), film);
             }
-        } else {
+        } else{
             throw new NotFoundException("по вашему id не был найден фильм");
         }
         return film;
