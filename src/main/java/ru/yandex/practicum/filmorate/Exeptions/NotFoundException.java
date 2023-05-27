e548d19520670bc75d7bbdd1f4cc0bb7a134101f
Николай баранович <nick.fakov@yandex.ru>
2023-05-27 18:08:51 +0300

    new branch add-friends-likes

--- src/main/java/ru/yandex/practicum/filmorate/Exeptions/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exeptions/NotFoundException.java
@@ -1,8 +1,9 @@
 package ru.yandex.practicum.filmorate.Exeptions;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
-public class NotFoundException extends IllegalArgumentException  {
+public class NotFoundException extends IllegalArgumentException {
     public NotFoundException(String message) {
         super(message);
         log.warn(message);
--- src/main/java/ru/yandex/practicum/filmorate/Exeptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exeptions/ValidationException.java
@@ -1,8 +1,9 @@
 package ru.yandex.practicum.filmorate.Exeptions;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
-public class ValidationException extends IllegalArgumentException  {
+public class ValidationException extends IllegalArgumentException {
     public ValidationException(String message) {
         super(message);
         log.warn(message);
