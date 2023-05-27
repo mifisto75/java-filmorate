9b18fb8ddbf2e93199228dbf2e5d31f40a216cac
Николай баранович <nick.fakov@yandex.ru>
2023-05-27 18:02:05 +0300

    new branch add-friends-likes

--- src/main/java/ru/yandex/practicum/filmorate/Exeptions/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exeptions/NotFoundException.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.Exeptions;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 @Slf4j
 public class NotFoundException extends IllegalArgumentException  {
     public NotFoundException(String message) {
