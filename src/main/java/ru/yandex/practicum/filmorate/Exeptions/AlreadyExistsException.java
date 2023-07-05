9185e2297bcf820f1e5e50ebe8ac9b96168d1c31
CheTigor <chetvertakov.igor@yandex.ru>
2023-07-05 03:01:00 +0300

    style(AlreadyExistsException.java): поправил стиль для проверки

--- src/main/java/ru/yandex/practicum/filmorate/Exeptions/AlreadyExistsException.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exeptions/AlreadyExistsException.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.Exeptions;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class AlreadyExistsException extends IllegalArgumentException{
+public class AlreadyExistsException extends IllegalArgumentException {
     public AlreadyExistsException(String message) {
         super(message);
         log.warn(message);
