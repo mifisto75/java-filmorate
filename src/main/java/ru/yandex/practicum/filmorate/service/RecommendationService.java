21c4747f6825945d772b8ea5d44b46d4bf202468
Юлия Герасименко <juliakoni@yandex.ru>
2023-07-06 20:47:28 +0300

    fix: исправлена ошибка рекомендации для пользователей с одинаковыми лайками

--- src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
@@ -13,8 +13,6 @@ import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static java.lang.String.format;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -27,10 +25,8 @@ public class RecommendationService {
         List<Like> allLikes = jdbcTemplate.query("SELECT * FROM film_like_list", this::rsToLike);
         // получаем список id всех пользователей и список id фильмов, который они пролайкали.
         Map<Integer, List<Integer>> usersAndLikedFilmsIds = makeMapOfUsersAndLikes(allLikes);
-        // получаем список id фильмов, которые пролайкал наш пользователь.
-        List<Integer> userLikedFilmsIds = getLikesForUser(id);
         // получаем список рекомендаций для пользователя
-        List<Integer> listOfRecommendedFilmsIds = makeRecommendationsList(usersAndLikedFilmsIds, userLikedFilmsIds);
+        List<Integer> listOfRecommendedFilmsIds = makeRecommendationsList(usersAndLikedFilmsIds, id);
 
         if (listOfRecommendedFilmsIds.size() < 1) {
             return Collections.emptyList();
@@ -45,82 +41,57 @@ public class RecommendationService {
 
     private List<Integer> makeRecommendationsList(
             Map<Integer, List<Integer>> usersAndLikedFilmsIds,
-            List<Integer> userLikedFilmsIds) {
+            int userId) {
         // создаем и заполняем список id пользователей и количества совпадений по лайкам с нашим пользователем.
+        List<Integer> userLikedFilmsIds = usersAndLikedFilmsIds.get(userId);
         Map<Integer, Long> idsAndMatchesCount = new HashMap<>();
 
-        for (int id : usersAndLikedFilmsIds.keySet()) {
-            List<Integer> user = userLikedFilmsIds;
-            user.retainAll(usersAndLikedFilmsIds.get(id));
-            long count = user.size();
-            if (user.isEmpty()) {
+        for (int otherUserId : usersAndLikedFilmsIds.keySet()) {
+            if (otherUserId == userId) {
                 continue;
             }
-            idsAndMatchesCount.put(id, count);
+            List<Integer> userLikes = new ArrayList<>(userLikedFilmsIds);
+            userLikes.retainAll(usersAndLikedFilmsIds.get(otherUserId));
+            if (userLikes.isEmpty()) {
+                continue;
+            }
+            long commonLikesCount = userLikes.size();
+            idsAndMatchesCount.put(otherUserId, commonLikesCount);
         }
 
         if (idsAndMatchesCount.isEmpty()) {
             return Collections.emptyList();
         }
-        // Найти 10 пользователей с максимальным количеством пересечения по лайкам.
-        Map<Integer, Long> topTenMatchesUsersIds = idsAndMatchesCount.entrySet().stream()
-                .sorted(Comparator.comparingLong(e -> -e.getValue())).limit(10)
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (a, b) -> {
-                            throw new AssertionError();
-                        },
-                        LinkedHashMap::new
-                ));
+        // Найти топ 10 пользователей с максимальным количеством пересечения по лайкам.
+        List<Integer> topTenMatchesUsersIds = idsAndMatchesCount.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(10)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
 
         // Определить id фильмов, которые наш пользователь не лайкал.
         List<Integer> recommendedFilmIds = new ArrayList<>();
-        for (int id : topTenMatchesUsersIds.keySet()) {
-            List<Integer> currentUserLikes = getLikesForUser(id);
-            List<Integer> notLikedFilmIds = setUniqueFilmIds(currentUserLikes, userLikedFilmsIds);
-            for (int i : notLikedFilmIds) {
-                if (!recommendedFilmIds.contains(i)) {
-                    recommendedFilmIds.add(i);
-                }
-            }
+        for (int otherUserId : topTenMatchesUsersIds) {
+            List<Integer> otherUserLikes = new ArrayList<>(usersAndLikedFilmsIds.get(otherUserId));
+            otherUserLikes.removeAll(userLikedFilmsIds);
+            otherUserLikes.removeAll(recommendedFilmIds);
+            recommendedFilmIds.addAll(otherUserLikes);
         }
         return recommendedFilmIds;
     }
 
-    public List<Integer> setUniqueFilmIds(List<Integer> one, List<Integer> two) {
-        List<Integer> notLikedFilmIds = new ArrayList<>();
-        for (int i : one) {
-            if (!two.contains(i)) {
-                notLikedFilmIds.add(i);
-            }
-        }
-        return notLikedFilmIds;
+    private Map<Integer, List<Integer>> makeMapOfUsersAndLikes(List<Like> allLikes) {
+        return allLikes.stream()
+                .collect(Collectors.toMap(
+                        Like::getUserId,
+                        like -> List.of(like.getFilmId()),
+                        this::concatenate
+                ));
     }
 
-    public List<Integer> getLikesForUser(int userId) {
-        return jdbcTemplate.queryForList(format(
-                "SELECT film_id FROM film_like_list WHERE user_id=%d", userId), Integer.class);
-    }
-
-    private Map<Integer, List<Integer>> makeMapOfUsersAndLikes(List<Like> likesList) {
-        Map<Integer, List<Integer>> usersAndTheirLikes = new HashMap<>();
-
-        for (Like like : likesList) {
-            int userId = like.getUserId();
-            int filmId = like.getFilmId();
-            if (!usersAndTheirLikes.containsKey(userId)) {
-                usersAndTheirLikes.put(userId, new ArrayList<>());
-                List<Integer> list = usersAndTheirLikes.get(userId);
-                list.add(filmId);
-                usersAndTheirLikes.put(userId, list);
-            } else {
-                List<Integer> list = usersAndTheirLikes.get(userId);
-                list.add(filmId);
-                usersAndTheirLikes.put(userId, list);
-            }
-        }
-        return usersAndTheirLikes;
+    private <T> List<T> concatenate(List<T> first, List<T> second) {
+        List<T> result = new ArrayList<>(first);
+        result.addAll(second);
+        return result;
     }
 
     private Like rsToLike(ResultSet rs, int rowNum) throws SQLException {
